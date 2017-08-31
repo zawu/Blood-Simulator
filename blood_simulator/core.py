c2f5f03225f056c387d892cfd5a2b1c903e6d6b6
Zack Wu <zacharywu7@gmail.com>
2017-08-31 15:31:22 -0800

    Base UI working, next step will be to connect to actual user inputs / calculations and move off of dummy events

--- blood_simulator/core.py
+++ blood_simulator/core.py
@@ -1,15 +1,58 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+#For mapping the graph
+import matplotlib
+matplotlib.use('TkAgg')
+
+from Tkinter import *
 import os,sys,csv
 
 #Import the activity class
 sys.path.append(os.path.abspath("activity.py"))
 import activity
 
+
 class BloodSimulator:
 
-	def __init__(self):
+	def __init__(self,window):
+		file_path = os.path.join(os.path.dirname(__file__))
+		self.exercise_dict = self.get_activity_dict(file_path + '/exercise.csv')
+		self.food_dict = self.get_activity_dict(file_path + '/food.csv')
+
+		#Level Frames
+		self.window = window
+		self.window.title("Blood Sugar Simulator")
+		self.mainframe = Frame(window)
+		self.mainframe.grid(column=0,row=0, sticky=(N,W,E,S), pady = 50, padx = 50)
+		self.mainframe.columnconfigure(0, weight = 1)
+		self.mainframe.rowconfigure(0, weight = 1)
+
+		self.tkvar = StringVar(self.mainframe)
+		self.tkvar.set('Choose an Exercise')
+		self.exercise_label = Label(self.mainframe, text="Choose an Exercise")
+		self.exercise_label.grid(row = 2, column =1)
+		self.exercise_menu = OptionMenu(self.mainframe, self.tkvar, *self.exercise_dict.keys())
+		self.exercise_menu.config(width=30)
+		self.exercise_menu.grid(row = 2, column =2)
+		self.exercise_time_input = Entry(self.mainframe,width=4)
+		self.exercise_time_input.insert(END, '1200')
+		self.exercise_time_input.grid(row = 2, column =3)
+		self.exercise_button = Button(self.mainframe, text="Add Exercise", width=10) 
+		self.exercise_button.grid(row = 2, column =4)
+
+		self.tkvar2 = StringVar(self.mainframe)
+		self.tkvar2.set('Choose a Food')
+		self.food_menu_label = Label(self.mainframe, text="Choose a Food")
+		self.food_menu_label.grid(row = 3, column =1)
+		self.food_menu = OptionMenu(self.mainframe, self.tkvar2, *self.food_dict.keys())
+		self.food_menu.config(width=30)
+		self.food_menu.grid(row = 3, column =2)
+		self.food_time_input = Entry(self.mainframe,width=4)
+		self.food_time_input.insert(END, '1200')
+		self.food_time_input.grid(row = 3, column =3)
+		self.food_button = Button(self.mainframe, text="Add Food", width=10)
+		self.food_button.grid(row = 3, column =4)
 
 	def get_activity_dict(self,csv_file):
 	    #Read in csv file 
@@ -33,7 +76,9 @@ class BloodSimulator:
 
 
 def main(): 
-
+    root = Tk()
+    app = BloodSimulator(root)
+    root.mainloop()
 
 if __name__ == '__main__':
-    main()	
+    main()		
